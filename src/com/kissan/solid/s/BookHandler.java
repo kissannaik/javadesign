bc344df5baf5603d304ea11c9900d723e4cca311
Kissan Naik <kissannaik@yahoo.com>
2020-08-29 11:52:53 +0530

    Single Responsibility Principle changes

---
+++ src/com/kissan/solid/s/BookHandler.java
@@ -0,0 +1,5 @@
+package com.kissan.solid.s;
+
+public interface BookHandler {
+    void save();
+}
---
+++ src/com/kissan/solid/s/BookPersistence.java
@@ -0,0 +1,9 @@
+package com.kissan.solid.s;
+
+public class BookPersistence {
+
+    public void save(Book book){
+        System.out.println("Saving book = " + book);
+    }
+
+}
